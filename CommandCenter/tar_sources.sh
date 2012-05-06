9453ea5f67a3e5a2c787bd63ef156944d1e9df1b
asksven <sven.knispel@gmail.com>
2012-05-06 22:04:49 +0200

    Updated to latest AdMob SDK

--- CommandCenter/tar_sources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d ./SystemSettings ]; then 
-  VERSION=`cat SystemSettings/AndroidManifest.xml | grep "versionName" | sed 's/"/ /g' | awk {'print $2'}`
-  tar -czf SystemSettings_src_${VERSION}.tar.gz ./SystemSettings/ --exclude=./SystemSettings/bin/*
-  tar -czf SystemSettings_${VERSION}.tar.gz ./SystemSettings/bin/SystemSettings.apk
-else
-  echo "you must be in your workspace dir to tar sources"
-fi
